c0d584fdfa726e3747d877a3738ee599fe46e5c4
vkuzel <vkuzel@gmail.com>
2023-01-24 08:15:45 +0100

    Extract signature methods.

--- src/main/java/xmldsig/XMLDSIGSigner.java
+++ src/main/java/xmldsig/XMLDSIGSigner.java
@@ -10,6 +10,8 @@ import javax.xml.crypto.dsig.keyinfo.KeyInfo;
 import javax.xml.crypto.dsig.keyinfo.KeyInfoFactory;
 import javax.xml.crypto.dsig.keyinfo.X509Data;
 import javax.xml.crypto.dsig.spec.C14NMethodParameterSpec;
+import javax.xml.crypto.dsig.spec.DigestMethodParameterSpec;
+import javax.xml.crypto.dsig.spec.SignatureMethodParameterSpec;
 import javax.xml.crypto.dsig.spec.TransformParameterSpec;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.NoSuchAlgorithmException;
@@ -20,10 +22,17 @@ import java.util.List;
 
 public class XMLDSIGSigner {
 
-    private static final String TYP_ALGORITHM = "http://www.w3.org/2000/09/xmldsig#enveloped-signature";
-    private static final String CAN_ALGORITHM = "http://www.w3.org/2006/12/xml-c14n11#WithComments";
-    private static final String DIG_ALGORITHM = "http://www.w3.org/2001/04/xmlenc#sha256";
-    private static final String SIG_ALGORITHM = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256";
+    // Removes "enveloped signature" from a document, so the signature element itself is not digested
+    private static final String TRANSFORM_ALGORITHM = "http://www.w3.org/2000/09/xmldsig#enveloped-signature";
+    // Canonicals (normalizes) a document. Preserves comments. E.g. removes line feeds, normalizes attributes, CDATA, etc.
+    private static final String CANONICALIZATION_ALGORITHM = "http://www.w3.org/2006/12/xml-c14n11#WithComments";
+    private static final String DIGEST_ALGORITHM = "http://www.w3.org/2001/04/xmlenc#sha256";
+    private static final String SIGN_ALGORITHM = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256";
+
+    private static final C14NMethodParameterSpec EMPTY_C14N_PARAMS = null;
+    private static final DigestMethodParameterSpec EMPTY_DIGEST_PARAMS = null;
+    private static final SignatureMethodParameterSpec EMPTY_SIGN_PARAMS = null;
+    private static final TransformParameterSpec EMPTY_TRANSFORM_PARAMS = null;
 
     private final Certificate certificate;
     private final PrivateKey privateKey;
@@ -55,30 +64,13 @@ public class XMLDSIGSigner {
     public Document signEnveloped(Document document) {
         try {
             XMLSignatureFactory xmlSignatureFactory = XMLSignatureFactory.getInstance("DOM", "XMLDSig");
-            CanonicalizationMethod c14nMethod = xmlSignatureFactory.newCanonicalizationMethod(CAN_ALGORITHM, (C14NMethodParameterSpec) null);
-            DigestMethod digestMethod = xmlSignatureFactory.newDigestMethod(DIG_ALGORITHM, null);
-            SignatureMethod signMethod = xmlSignatureFactory.newSignatureMethod(SIG_ALGORITHM, null);
-            Transform sigTransform = xmlSignatureFactory.newTransform(TYP_ALGORITHM, (TransformParameterSpec) null);
-            Transform canTransform = xmlSignatureFactory.newTransform(CAN_ALGORITHM, (TransformParameterSpec) null);
 
-            List<Transform> transforms = List.of(sigTransform, canTransform);
-
-            // Empty URI points to the root element. Otherwise, the URI would have to point to a signed element.
-            Reference referenceDoc = xmlSignatureFactory.newReference("", digestMethod, transforms, null, null);
-
-            List<Reference> references = List.of(referenceDoc);
-
-            SignedInfo signedInfo = xmlSignatureFactory.newSignedInfo(c14nMethod, signMethod, references);
-
-            KeyInfoFactory keyInfoFactory = xmlSignatureFactory.getKeyInfoFactory();
-            X509Data x509Data = keyInfoFactory.newX509Data(List.of(certificate));
-            KeyInfo keyInfo = keyInfoFactory.newKeyInfo(List.of(x509Data));
-
+            SignedInfo signedInfo = createSignedInfo(xmlSignatureFactory);
+            KeyInfo keyInfo = createKeyInfo(xmlSignatureFactory);
             XMLSignature xmlSignature = xmlSignatureFactory.newXMLSignature(signedInfo, keyInfo, null, null, null);
+
             Element rootNode = document.getDocumentElement();
-
             DOMSignContext domSignContext = new DOMSignContext(privateKey, rootNode);
-
             xmlSignature.sign(domSignContext);
 
             return document;
@@ -86,5 +78,31 @@ public class XMLDSIGSigner {
                  XMLSignatureException | NoSuchProviderException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static SignedInfo createSignedInfo(XMLSignatureFactory xmlSignatureFactory) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException {
+        CanonicalizationMethod c14nMethod = xmlSignatureFactory.newCanonicalizationMethod(CANONICALIZATION_ALGORITHM, EMPTY_C14N_PARAMS);
+        DigestMethod digestMethod = xmlSignatureFactory.newDigestMethod(DIGEST_ALGORITHM, EMPTY_DIGEST_PARAMS);
+        SignatureMethod signMethod = xmlSignatureFactory.newSignatureMethod(SIGN_ALGORITHM, EMPTY_SIGN_PARAMS);
+
+        // Before calculating digest (hash) the document is transformed into
+        // its canonical (normalized) form so the digest is consistent even
+        // if document is reformatted, etc.
+        List<Transform> transforms = List.of(
+                xmlSignatureFactory.newTransform(TRANSFORM_ALGORITHM, EMPTY_TRANSFORM_PARAMS),
+                xmlSignatureFactory.newTransform(CANONICALIZATION_ALGORITHM, EMPTY_TRANSFORM_PARAMS)
+        );
+
+        // Empty URI points to the root element. Otherwise, the URI would have to point to a signed element.
+        Reference referenceDoc = xmlSignatureFactory.newReference("", digestMethod, transforms, null, null);
+        List<Reference> references = List.of(referenceDoc);
+
+        return xmlSignatureFactory.newSignedInfo(c14nMethod, signMethod, references);
+    }
+
+    private KeyInfo createKeyInfo(XMLSignatureFactory xmlSignatureFactory) {
+        KeyInfoFactory keyInfoFactory = xmlSignatureFactory.getKeyInfoFactory();
+        X509Data x509Data = keyInfoFactory.newX509Data(List.of(certificate));
+        return keyInfoFactory.newKeyInfo(List.of(x509Data));
     }
 }
