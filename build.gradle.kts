9765ed8039662c279501d49c116e2f510c960c19
vkuzel <vkuzel@gmail.com>
2023-02-04 13:04:44 +0100

    Remove unused dependencies (Bouncy Castle).

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,12 +22,7 @@ repositories {
 }
 
 dependencies {
-    implementation("jakarta.xml.bind:jakarta.xml.bind-api:2.3.3")
-    implementation("javax.xml.bind:jaxb-api:2.3.1")
     implementation("org.glassfish.jaxb:jaxb-runtime:2.3.6")
-    implementation("org.bouncycastle:bcprov-jdk18on:1.72")
-    implementation("org.bouncycastle:bcpkix-jdk18on:1.72")
-    implementation("org.bouncycastle:bcmail-jdk18on:1.72")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
 
