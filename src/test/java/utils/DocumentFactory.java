f6c9e00f2584edd0df50762cee7b01b1e655ba05
vkuzel <vkuzel@gmail.com>
2023-02-08 20:07:36 +0100

    Fix typo.

--- src/test/java/utils/DocumentFactory.java
+++ src/test/java/utils/DocumentFactory.java
@@ -5,31 +5,22 @@ import https.github_com.vkuzel.xades_demo.SingableDocumentType;
 import org.w3c.dom.Document;
 
 import javax.xml.bind.JAXBElement;
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
-import javax.xml.datatype.XMLGregorianCalendar;
 import java.io.IOException;
 import java.io.InputStream;
-import java.math.BigInteger;
-import java.time.ZonedDateTime;
-import java.util.GregorianCalendar;
 
 import static document.DocumentTransformer.fromBytes;
 import static document.DocumentTransformer.toDocument;
-import static java.math.BigInteger.ONE;
 import static java.util.Objects.requireNonNull;
 
 public class DocumentFactory {
 
-    public static final BigInteger NUMERIC_VALUE = ONE;
     public static final String SOME_VALUE = "some-value";
-    public static final XMLGregorianCalendar TIME_VALUE = xmlGregorianCalendar("2000-01-01T01:01:01Z");
 
     public static Document createDocumentToSign() {
         ObjectFactory objectFactory = new ObjectFactory();
-        SingableDocumentType signableDocument = objectFactory.createSingableDocumentType();
-        signableDocument.setSomeElement(SOME_VALUE);
-        JAXBElement<SingableDocumentType> docToSign = objectFactory.createSingableDocument(signableDocument);
+        SingableDocumentType singableDocument = objectFactory.createSingableDocumentType();
+        singableDocument.setSomeElement(SOME_VALUE);
+        JAXBElement<SingableDocumentType> docToSign = objectFactory.createSingableDocument(singableDocument);
         return toDocument(docToSign);
     }
 
@@ -54,15 +45,6 @@ public class DocumentFactory {
             byte[] content = requireNonNull(inputStream).readAllBytes();
             return fromBytes(content);
         } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static XMLGregorianCalendar xmlGregorianCalendar(String isoDateTime) {
-        try {
-            GregorianCalendar gregorianCalendar = GregorianCalendar.from(ZonedDateTime.parse(isoDateTime));
-            return DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar);
-        } catch (DatatypeConfigurationException e) {
             throw new RuntimeException(e);
         }
     }
